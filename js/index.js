16dfb87fe1f4c78f4988fdc8fbf12d6984c4a9d6
Temouche <temi.kergastel@gmail.com>
2025-12-17 16:48:41 +0100

    projets

---
+++ js/index.js
@@ -0,0 +1,21 @@
+// Gère l'apparition du bouton menu flottant
+document.addEventListener('DOMContentLoaded', function() {
+    
+    const menuFlottant = document.getElementById('menuFlottant');
+    const navElement = document.querySelector('nav');
+    
+    window.addEventListener('scroll', () => {
+        if (navElement) {
+            const navRect = navElement.getBoundingClientRect();
+            const isNavOutOfView = navRect.bottom < 0;
+            
+            if (isNavOutOfView) {
+                menuFlottant.classList.remove('opacity-0', 'pointer-events-none');
+                menuFlottant.classList.add('opacity-100');
+            } else {
+                menuFlottant.classList.add('opacity-0', 'pointer-events-none');
+                menuFlottant.classList.remove('opacity-100');
+            }
+        }
+    });
+});
